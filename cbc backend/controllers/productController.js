d490a6da4e3fb3c33f249d8a8bd1d9dacb46bce9
Samadhi <samadhipoorni98@gmail.com>
2025-05-29 00:31:31 +0530

    Built Create &  Get Functions for Products🔨📄

--- cbc backend/controllers/productController.js
+++ cbc backend/controllers/productController.js
@@ -1,3 +1,31 @@
-import function createProduct(req,res){
+import product from "../models/ products.js";
+
+
+export function createProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.json({
+            message: "Please login as administrator to create products"
+        })
+        return
+    }
     
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>(
+        res.json({
+            message: "Product created"
+        })
+    )).catch((error)=>(
+        res.json({
+            message: "Product not created"
+        })
+    ))
+}
+export function getProduct(req,res){
+    Product.find({}).then((products)=>{
+        res.json(products)
+    })
 }
