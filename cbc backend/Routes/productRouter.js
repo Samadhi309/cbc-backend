19cf66caf06ec49a3edf5b3cab607ce68cf307c4
Samadhi <samadhipoorni98@gmail.com>
2025-05-29 00:55:28 +0530

    Create productRouter

---
+++ cbc backend/Routes/productRouter.js
@@ -0,0 +1,6 @@
+import express from  "express";
+import { createProduct, getProduct } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+productRoute.post("/",createProduct)
+productRoute.get("/",getProduct)
