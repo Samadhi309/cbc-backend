8bfd403fd4484d37bb57fb460c229baaf149b26a
Samadhi <samadhipoorni98@gmail.com>
2025-05-29 01:06:21 +0530

    Connect product router to index.js

--- cbc backend/Routes/productRouter.js
+++ cbc backend/Routes/productRouter.js
@@ -4,3 +4,5 @@ import { createProduct, getProduct } from "../controllers/productController.js";
 const productRouter = express.Router();
 productRoute.post("/",createProduct)
 productRoute.get("/",getProduct)
+
+export default productRouter;
