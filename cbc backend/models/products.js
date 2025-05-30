aacfd0b1a9b899b3c96ebd8ed5ca4ee17842ec72
Samadhi <samadhipoorni98@gmail.com>
2025-05-30 12:51:26 +0530

    Corrected typo in product model

--- cbc backend/models/products.js
+++ cbc backend/models/products.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.schema({
+const productSchema = mongoose.Schema({
     productId : {
         type : String,
         required : true,
